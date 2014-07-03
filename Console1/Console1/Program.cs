fa1651fcd6b93e9d7900f7cda913367fe8f1eb06
Falcon760 <sfalcon266@hotmail.com>
2014-07-03 09:38:26 -0400

    Added

---
+++ Console1/Console1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Maththing
+    {
+        public int larger;
+
+        public int Compare(int x, int y)
+        {
+            int result;
+            if (x > y)
+                larger = y;
+            else
+                larger = x;
+            return result=larger;
+
+        }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+      
+        {
+           int abc = 500;
+           int xyz = 200;
+
+            // int.Parse(Console.ReadLine());
+           Maththing math = new Maththing();
+           var Blah = math.Compare(abc, xyz);
+        
+            Console.WriteLine(Blah);
+            Console.ReadLine();
+
+        }
+
+    }
+
+    
+
+
+            
+    }
+
+}
+
+
---
+++ Console1/Console2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book();
+            book.Price = 9.99M;
+            var second_book = new Book();
+            second_book.Price = 6.99M;
+            if (book.CompareTo(second_book) == 0)
+                Console.WriteLine("Books are the same Price");
+            else if (book.CompareTo(second_book) == 1)
+                Console.WriteLine("First Book is higher price than Second book");
+            else if (book.CompareTo(second_book) == -1)
+                Console.WriteLine("Second book is higher price than first book");
+            Console.ReadLine();
+
+        }
+        public class Book : IComparable
+        {
+            private decimal price;
+            public decimal Price
+                //double holds longer scientific calculations
+                //decimal
+                
+
+
+            {
+                get {return price;}
+                set {if (value >= 0) price = value;}
+            }
+            public int CompareTo(object obj)
+            {
+                if (obj is Book)
+                {
+                    if (this.Price == (obj as Book).Price)
+                    {
+                        return 0;
+                    }
+                    else if (this.Price > (obj as Book).Price)
+                    {
+                        return 1;
+                    }
+
+                    else if (this.Price < (obj as Book).Price)
+                    {
+                        return -1;
+                    }
+
+                }
+
+                return -1;
+            }
+        }
+    }
+}
